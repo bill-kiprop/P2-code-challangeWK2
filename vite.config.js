0a7dc788f1e6fe427aa3b99bf74b4d2f1f997ba2
billkiprop <kipropbill3@gmail.com>
2024-05-11 02:10:09 +0300

    bugs fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+
+
+  build: {
+    outDir: resolve(process.cwd(), 'build')// Specify the output directory
+  },
+
   plugins: [react()],
 })
